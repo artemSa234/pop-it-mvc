28f6ad8f7349bae8af8ad8a9ffee488aecbbd796
Artem <156803343+artemSa234@users.noreply.github.com>
2024-03-19 18:14:02 +0700

    Функция регистрации

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -3,13 +3,15 @@
 namespace Controller;
 
 use Model\Post;
+use Src\Request;
 use Src\View;
+use Model\User;
 
 class Site
 {
-   public function index(): string
+    public function index(Request $request): string
    {
-       $posts = Post::all();
+    $posts = Post::where('id', $request->id)->get();
        return (new View())->render('site.post', ['posts' => $posts]);
    }
 
@@ -17,4 +19,12 @@ class Site
    {
        return new View('site.hello', ['message' => 'hello working']);
    }
+   public function signup(Request $request): string
+   {
+       if ($request->method==='POST' && User::create($request->all())){
+           return new View('site.signup', ['message'=>'Вы успешно зарегистрированы']);
+       }
+       return new View('site.signup');
+   }
+ 
 }
