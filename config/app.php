d5dd59e439a2cbbb6c9724126a4e0b032a86a547
Artem <156803343+artemSa234@users.noreply.github.com>
2024-03-20 18:10:11 +0700

    Базовая защита от XSS-атак,Базовая защита от CSRF-атак

--- config/app.php
+++ config/app.php
@@ -14,5 +14,16 @@ return [
    'validators' => [
         'required' => \Validators\RequireValidator::class,
         'unique' => \Validators\UniqueValidator::class
-    ]
+   ],
+   'routeAppMiddleware' => [
+    'trim' => \Middlewares\TrimMiddleware::class,
+    'specialChars' => \Middlewares\SpecialCharsMiddleware::class,
+ ],
+ 'routeAppMiddleware' => [
+    'csrf' => \Middlewares\CSRFMiddleware::class,
+    'trim' => \Middlewares\TrimMiddleware::class,
+    'specialChars' => \Middlewares\SpecialCharsMiddleware::class,
+ ],
+ 
+ 
 ];
